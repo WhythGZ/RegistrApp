fc7d5d8ee30b28f0fb1a18511e5b08c4f478486c
Whyth <101737722+WhythGZ@users.noreply.github.com>
2022-09-07 10:17:08 -0300

    update

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  usuario = {
+    name: '',
+    suname: '',
+    username: '',
+    password: '',
+    email: '',
+  }
+
+  constructor(private router: Router, private toastController: ToastController) { }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', message, icon){
+    const toast = await this.toastController.create({
+      message,
+      duration: 1500,
+      position,
+      icon,
+    });
+
+    await toast.present();
+  }
+
+  validarForm(){
+    const flag = document.getElementById('form').textContent;
+    if (flag === 'true'){
+      this.presentToast('bottom', 'Verifique su email', 'mail-sharp');
+    }
+    else{
+      this.presentToast('bottom', 'Debe rellenar los campos', 'alert-circle-sharp');
+    }
+}
+
 
   ngOnInit() {
   }
